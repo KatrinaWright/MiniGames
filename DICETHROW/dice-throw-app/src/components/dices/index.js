22490cd9359bbcebe4060873e23fcd09204eaf67
KatrinaWright <katrina.elise.wright@gmail.com>
2023-07-06 22:21:24 -0400

    adding game logic pt  1

--- DICETHROW/dice-throw-app/src/components/dices/index.js
+++ DICETHROW/dice-throw-app/src/components/dices/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 import './index.css';
-import dice1 from '../../images/dice1.svg';
 
 function Dices() {
+    const user1 = useRef(null);
+  const user2 = useRef(null);
+
+  let diceImages = [
+    '../images/slime.png',
+    '../images/logo192.png',
+    '../images/logo512.png',
+    // '../images/dice4.svg',
+    // '../images/dice5.svg',
+    // '../images/dice6.svg',
+  ];
+
+  const firstRandomNum = Math.floor(Math.random() * 6);
+  const secondRandomNum = Math.floor(Math.random() * 6);
+
+  if (firstRandomNum > secondRandomNum) {
+    console.log(`The winner is user 1 with ${firstRandomNum + 1} points`);
+  } else if (firstRandomNum < secondRandomNum) {
+    console.log(`The winner is user 2 with ${secondRandomNum + 1} points`);
+  } else {
+    console.log(`It's a draw!`);
+  }
+
+  console.log(firstRandomNum, secondRandomNum);
+
   /*
   // Generate a random number 1 to 6
   const firstRandomNum = Math.floor(Math.random() * 6) + 1;
@@ -29,21 +53,24 @@ function Dices() {
   }
   roll();
   */
-
+    const roll = () => {
+    user1.current.setAttribute('src', '/images/slime.png');
+    user2.current.setAttribute('src', '/images/logo192.png');
+  };
   return (
     <>
       <div className='dice-wrapper'>
         <div className='dice-area'>
           <p>User 1</p>
-          <img src={dice1} />
+          <img ref={user1} />
         </div>
         <div className='dice-area'>
           <p>User 2</p>
-          <img src={dice1} />
+          <img ref={user2} />
         </div>
       </div>
       <p className='result'>The winner is user X with Y points</p>
-      <button>Roll the dice</button>
+      <button onClick={roll}>Roll the dice</button>
     </>
   );
 }
